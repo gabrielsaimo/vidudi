ccaecc9737459684018e4b8d96219d6468081f52
gabrielsaimo <gabrielsaimo68@gmail.com>
2021-10-07 20:09:41 -0300

    correção

--- form/home.php
+++ form/home.php
@@ -1,22 +1,99 @@
-<!DOCTYPE html>
-<html lang="en">
+
 
 <head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <link rel="stylesheet" type="text/css" href="../css/mobile.css">
 </head>
 
-<body>
-    <form style="margin-top: 437px;margin-bottom: -450px; cursor: pointer;">
+
+<style>
+  
+   
+@media only screen and (min-width: 600px){
+    .imagem {
+    width: 280px !important;
+    border-radius: 50%;
+    display: block;
+    margin-left: auto;
+    margin-right: auto;
+}
+.btn1 {
+    display: block;
+    margin-left: auto;
+    margin-right: auto;
+    transition: .4s;
+    width: 75% !important;
+    height: 100px;
+    font-size: 45px !important;
+    text-transform: uppercase;
+    letter-spacing: 2.5px;
+    font-weight: 500;
+    color: #000;
+    background-color: #094679;
+    border: none;
+    border-radius: 20px;
+    box-shadow: 0px 8px 15px rgb(0 0 0 / 10%);
+    transition: all 0.3s ease 0s;
+    cursor: pointer;
+    outline: none;
+}td.texto{  /*** futurista esquerda ****/
+    display: flex;
+    text-align: center;
+    align-items: center;
+    justify-content: center;
+    width: 450px;
+    height: 150px;
+    margin-top: 1px;
+    font-size: 45px;
+    text-transform: uppercase;
+    letter-spacing: 2.5px;
+    font-weight: 500;
+    color: #000;
+    margin-right: -25px;
+    background-color: rgb(185, 177, 177);
+    border: none;
+    border-radius: 45px 10px 10px 40px;
+    box-shadow:0px 0px 50px 10px rgba(0, 0, 0, 0.1);
+    transition: all 0.3s ease 0s;
+    cursor:default;
+    outline: none;
+}.input1{  /*** futurista direita ****/
+    display: flex;
+    text-align: center;
+    align-items: center;
+    justify-content: center;
+    width: 450px;
+    height: 150px;
+    font-size: 40px;
+    text-transform: uppercase;
+    letter-spacing: 1.2px;
+    font-weight: 500;
+    color: #000;
+    background-color: #fff;
+    border: 1px  solid rgb(220, 220, 220);
+    border-radius: 45px;
+    box-shadow:0px 0px 60px 10px rgba(0, 0, 0, 0.1);
+    transition: all 0.3s ease 0s;
+    outline: none;
+    z-index: 2;
+}
+input[type=checkbox] {
+    transform: scale(6);
+}
+.row{
+    margin-left: 50px;
+}
+}
+      
+   
+</style>
+    <form style="margin-top: 1000px; cursor: pointer;">
         <? $idusuario_cookie = $_COOKIE['idusuario'];
         $qp = $banco->query("SELECT p.* from pessoa p join usuario u on (p.idusuario = u.idusuario) where u.idusuario =" . $idusuario_cookie);
         $rows = mysqli_fetch_array($qp);
         $sqlbairro = $banco->query( "SELECT * FROM bairro");
         if (empty($rows['idlider'])) {
             $q = $banco->query("SELECT p.id,p.nome FROM pessoa p join emcargo e on(p.idemcargo = e.idemcargo) where p.idemcargo in(2,3,4) and p.status='ATIVO'") or die("erro ao selecionar"); ?>
-            <table class="centro menos-top">
+            <table class=" menos-top">
                 <tbody>
                     <? if (isset($rows["id"]) != null) { ?>
                         <? $pessoa = selectIdPessoa($rows["id"]); ?>
@@ -70,25 +147,25 @@
                     <tr>
                         <td>
                             <input type="checkbox" name="batizado" id="batizado">
-                            <label for="batizado">Bati</label>
+                            <label style="font-size: 70px; margin-left: 40px;" for="batizado">Batizado</label>
 
                         </td>
                     <tr>
                         <td>
                             <input type="checkbox" name="cursao" id="cursao">
-                            <label for="cursao">Cursao</label>
+                            <label style="font-size: 70px; margin-left: 40px;" for="cursao">Cursao</label>
                         </td>
                     </tr>
                     <tr>
                         <td>
                             <input type="checkbox" name="ctl" id="ctl">
-                            <label for="ctl">CTL</label>
+                            <label style="font-size: 70px; margin-left: 40px;" for="ctl">CTL</label>
                         </td>
                     </tr>
                     <tr>
                         <td>
                             <input type="checkbox" name="semi" id="semi">
-                            <label for="semi">Seminario</label>
+                            <label style="font-size: 70px; margin-left: 40px;" for="semi">Seminario</label>
                         </td>
                     </tr>
 
@@ -101,10 +178,15 @@
                         <td><input type="hidden" name="_acao" value="u">
                         <td><input type="hidden" name="_modulo" value="pessoa">
                         <? } ?>
-                        <input type="hidden" name="id" value="<?= $pessoa["id"] ?> ">
-                        <input type="hidden" nome="idusuario" value="<?= $idusuario_cookie ?>">
+                            <input type="hidden" name="id" value="<?= $pessoa["id"] ?> ">
+                            <input type="hidden" nome="idusuario" value="<?= $idusuario_cookie ?>">
                         </td>
-                        <td><br><br><br><br><br><input class="enviar btn1 left" type="submit" name="Enviar" value="Enviar"></td>
+                        <tr>
+                        <td colspan=3>
+                            <input class="enviar btn1 left" type="submit" name="Enviar" value="Enviar">
+                        </td>
+                        </tr>
+                        </td>
                 </tbody>
             </table>
         <? } else { ?>
@@ -121,6 +203,5 @@
             }
         </script>
     </form>
-</body>
 
-</html>
+
--- form/pessoa.php
+++ form/pessoa.php
@@ -1,5 +1,85 @@
-<link rel="stylesheet" type="text/css" href="css/style.css">
+
+
+
 <meta charset="UTF-8">
+<head>
+    <link rel="stylesheet" type="text/css" href="../css/mobile.css">
+</head>
+<style>
+    <?if($celula == 1){?>
+   
+@media only screen and (min-width: 600px){
+    .imagem {
+    width: 280px !important;
+    border-radius: 50%;
+    display: block;
+    margin-left: auto;
+    margin-right: auto;
+}
+.btn1 {
+    display: block;
+    margin-left: auto;
+    margin-right: auto;
+    transition: .4s;
+    width: 75% !important;
+    height: 100px;
+    font-size: 45px !important;
+    text-transform: uppercase;
+    letter-spacing: 2.5px;
+    font-weight: 500;
+    color: #000;
+    background-color: #094679;
+    border: none;
+    border-radius: 20px;
+    box-shadow: 0px 8px 15px rgb(0 0 0 / 10%);
+    transition: all 0.3s ease 0s;
+    cursor: pointer;
+    outline: none;
+}.texto{  /*** futurista esquerda ****/
+    display: flex;
+    text-align: center;
+    align-items: center;
+    justify-content: center;
+    width: 450px;
+    height: 150px;
+    margin-top: 1px;
+    font-size: 45px;
+    text-transform: uppercase;
+    letter-spacing: 2.5px;
+    font-weight: 500;
+    color: #000;
+    margin-right: -25px;
+    background-color: rgb(185, 177, 177);
+    border: none;
+    border-radius: 45px 10px 10px 40px;
+    box-shadow:0px 0px 50px 10px rgba(0, 0, 0, 0.1);
+    transition: all 0.3s ease 0s;
+    cursor:default;
+    outline: none;
+}.input1{  /*** futurista direita ****/
+    display: flex;
+    text-align: center;
+    align-items: center;
+    justify-content: center;
+    width: 450px;
+    height: 150px;
+    font-size: 40px;
+    text-transform: uppercase;
+    letter-spacing: 1.2px;
+    font-weight: 500;
+    color: #000;
+    background-color: #fff;
+    border: 1px  solid rgb(220, 220, 220);
+    border-radius: 45px;
+    box-shadow:0px 0px 60px 10px rgba(0, 0, 0, 0.1);
+    transition: all 0.3s ease 0s;
+    outline: none;
+    z-index: 2;
+}
+}
+        <?}?>
+   
+</style>
 <div class="caixa"></div>
 <div class="container ">
     <br>
@@ -52,16 +132,16 @@
                     <? } ?>
                 </tr>
                 <tr>
-                    <td class="texto1">Sexo: </td>
+                    <td class="texto">Sexo: </td>
                     <? if (isset($_GET["id"]) != null) { ?>
                         <!--  _1_i_pessoa_sexo  ---->
-                        <td><select class="selects" name="sexo" value="">
+                        <td><select class='input1' name="sexo" value="">
                                 <option value="Homem">Homem</option>
                                 <option value="mulher">mulher</option>
                             </select>
                         </td>
                     <? } else { ?>
-                        <td><select class="selects" name="sexo" value="">
+                        <td><select class='input1' name="sexo" value="">
                                 <option value="Homem">Homem</option>
                                 <option value="mulher">mulher</option>
                             </select>
@@ -99,7 +179,13 @@
                     <input type="hidden" name="id" value="<?= $pessoa["id"] ?> ">
                     <input type="hidden" nome="idusuario" value="<?= $idusuario_cookie ?>">
                     </td>
-                    <td><br><br><br><br><br><input class="enviar btn1 left" type="submit" name="Enviar" value="Enviar"></td>
+                    <tr>
+                        <td>
+
+                        </td>
+                    </tr>
+                    <tr><td colspan=3><input class=" btn1 " type="submit" name="Enviar" value="Enviar"></td></tr>
+                    </td>
                     </tr>
             </tbody>
         </table>
