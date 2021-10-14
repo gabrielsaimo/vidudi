dadf74c1538211c5bd5bb6738b2d1eccbe7380bf
Gabriel Saimo Ferreira Espíndola <gabrielsaimo68@gmail.com>
2021-10-14 16:53:35 -0300

    correção dnovo

--- form/index.php
+++ form/index.php
@@ -56,17 +56,21 @@ if ($detect->isMobile()) {  //se o dispositivo é um dispositivo móvel
                         $qli = $banco->query("SELECT * From anexo where tipoanexo = 'cadapdf' order by idanexo desc") or die('erro ao buscar link');
                         
                         while ($rowli = mysqli_fetch_array($qli)) {
-                            echo '<div style="margin-bottom: 180px;display: block;margin-left: auto;margin-right: auto;width: 324px;height: 400px;">';
-                            echo '<img style="width: 370px; height: 450px; border-radius: 10%;" src="data:image/gif;base64,' . base64_encode($rowli['anexo']) . '"/>'; 
-                            $texto = $rowli['link'];
-                            $audio = $rowli['linkaudio'];
-                            $novo_texto = str_replace("file/d/", "u/0/uc?id=", $texto);
-                            $link = str_replace("/view?usp=sharing", "&export=download", $novo_texto);
-                            $novo_audio = str_replace("file/d/", "u/0/uc?id=", $audio);
-                            $linkaudio = str_replace("/view?usp=sharing", "&export=download", $novo_audio);
-                            ?><a style="display: block;width: 370px;  margin-top:5px" href="<?= $link ?>"><li type="button"  style="width: 370px;" class="btn1"> <?=$rowli['titulo']?><br> <?=date('d-m-Y', strtotime($rowli['data']))?> - PDF </li> </a> <?
-                          if(!empty($audio)){?> 
-                           <a style="display: block;width: 370px;  margin-top:5px" href="<?=$linkaudio?>"><li type="button"  style="width: 370px;" class="btn1"> <?=date('d-m-Y', strtotime($rowli['data']))?> - Audio </li> </a><?}
+                            if($rowli['anexo'] != ''){
+                                echo '<div style="margin-bottom: 180px;display: block;margin-left: auto;margin-right: auto;width: 355px;height: 400px;text-align: center;">'.$rowli['titulo'];
+                                echo '<img style="width: 370px; height: 450px; border-radius: 5%;" src="data:image/gif;base64,' . base64_encode($rowli['anexo']) . '"/>'; 
+                            }else{
+                                echo '<div style="margin-bottom: 30px;display: block;margin-left: auto;margin-right: auto;width: 355px;height: 100px;text-align: center;"> '.$rowli['titulo'];
+                            }
+                                $texto = $rowli['link'];
+                                $audio = $rowli['linkaudio'];
+                                $novo_texto = str_replace("file/d/", "u/0/uc?id=", $texto);
+                                $link = str_replace("/view?usp=sharing", "&export=download", $novo_texto);
+                                $novo_audio = str_replace("file/d/", "u/0/uc?id=", $audio);
+                                $linkaudio = str_replace("/view?usp=sharing", "&export=download", $novo_audio);
+                            if(!empty($texto)){?><a style="display: block;width: 370px;  margin-top:5px" href="<?= $link ?>"><li type="button"  style="width: 370px;" class="btn1"> <?=$rowli['titulo']?><br> <?=date('d-m-Y', strtotime($rowli['data']))?> - PDF </li> </a> <?
+                            }if(!empty($audio)){?> 
+                           <a style="display: block;width: 370px;  margin-top:5px" href="<?=$linkaudio?>"><li type="button"  style="width: 370px;" class="btn1"> <?=date('d-m-Y', strtotime($rowli['data']))?> - Audio</li> </a><?}
                             echo '</div>';
                         }
                         echo '</div>';
