03f9cb1f6769a23e730cfeeb6f527e6915078429
Gabriel Saimo Ferreira Espíndola <gabrielsaimo68@gmail.com>
2021-10-14 14:55:21 -0300

    up

--- form/gerenciar.php
+++ form/gerenciar.php
@@ -10,18 +10,18 @@ $qpf = $banco->query("SELECT * From anexo  ") or die('erro ao consultar'.mysqli_
 <br>
 <table>
     <tr>
-        <td class="texto" style="width: 50px;" >Link do PDF:</td>
+        <td class="texto" style="width: 105px;" >Link do PDF:</td>
         <td><input type="text" name="link" value="" class="input1" placeholder="link do google drive"></td>
     </tr>
     <tr>
-        <td class="texto" style="width: 50px;" >Link do audio:</td>
+        <td class="texto"  >Link do audio:</td>
         <td><input type="text" name="linkaudio" value="" class="input1" placeholder="link do google drive"></td>
     </tr>
     <tr>
-        <td>
-        <td class="texto" style="width: 50px;" >Data:</td>
+        
+        <td class="texto"  >Data:</td>
         <td><input type="date" name="data" value="" class="input1" ></td>
-        </td>
+        
     </tr>
     <tr>
                     <td colspan="2"><input id="submit" type="submit" class="btn1" style="display: block;
